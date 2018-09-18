b8f8eac06caf4762c3ec07720e03309313f60848
Edrard A <demonand@gmail.com>
2018-09-18 15:01:24 +0300

    micro fix

--- src/WgGetter/WgDataGetter.php
+++ src/WgGetter/WgDataGetter.php
@@ -42,7 +42,7 @@ class WgDataGetter
                 MyLog::error("Need to sleep ".($sleep),array(),'wgdata_g');
                 usleep($sleep);
             }
-            MyLog::debug("Run Time ".(microtime(true) - $start).' Executed URLS - '.count($this->curl->getSessions()),array(),'wgdata_g');
+            MyLog::debug("Run Time ".(microtime(true) - $start),array(count($this->curl->getSessions())),'wgdata_g');
             $request = $function !== NULL ? $function($request,$urls) : $request;
             if($instead === FALSE){
                 $request = $this->check($request,$urls);
